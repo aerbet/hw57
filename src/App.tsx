d2c132ca3d2c765af2e86f75fa82bdcd0458ef83
Erbol Zhaparov <ae270718@gmail.com>
2023-11-10 17:07:28 +0600

    added users component and completing app

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,23 @@ import './App.css';
 import React, { useState } from 'react';
 import UserForm from './components/UserForm/UserForm';
 import Users from './components/Users/Users';
+import {User} from '../types';
 
 const App: React.FC = () => {
-  const [users, setUsers] = useState<any[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
 
-  const addUser = (user: any) => {
+  const addUser = (user: User) => {
     setUsers((prevUsers) => [...prevUsers, user]);
   };
 
   return (
-    <>
-    </>
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col-md-6">
+          <Users users={users} />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,7 +1,14 @@
-const Users = () => {
+import React from 'react';
+import UserItem from '../UserItem/UserItem';
+import { UsersProps } from '../../../types';
+
+const Users: React.FC<UsersProps> = ({ users }) => {
   return (
     <div>
-
+      <h2>Users</h2>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
     </div>
   );
 };
